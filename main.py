1235c1e27dfd7ee4709e27b2a6ef38c05c32f3d3
Liam <ml646@duke.edu>
2023-11-29 21:21:33 -0500

    update cicd

--- main.py
+++ main.py
@@ -3,15 +3,17 @@ import requests
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
-@app.route('/process_form', methods=['POST'])
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/process_form", methods=["POST"])
 def process_form():
     # Get data from user input
-    user_input = request.form['user_input']
-    user_input +=",please speak in English"
+    user_input = request.form["user_input"]
+    user_input += ",please speak in English"
     url = "https://luckycola.com.cn/ai/openwxyy"
 
     args = {
@@ -21,18 +23,23 @@ def process_form():
         "appKey": "65653dd96dd7b2f885ab5dd1",
         # User Id
         "uid": "9nmGuQ17011337516758P97ncXnIO",
-        "isLongChat": 0
+        "isLongChat": 0,
     }
 
     try:
         response = requests.post(url, json=args)
         response.raise_for_status()  # Raise an HTTPError for bad responses
-        result = response.json().get('data', {}).get('result', 'None')
+        result = response.json().get("data", {}).get("result", "None")
     except requests.RequestException as e:
         result = f"Error: {str(e)}"
 
     print("Response:", result)
-    return render_template('result.html', response=result), 200, {'Content-Type': 'text/html; charset=utf-8'}
+    return (
+        render_template("result.html", response=result),
+        200,
+        {"Content-Type": "text/html; charset=utf-8"},
+    )
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=8080,debug=True)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True)
